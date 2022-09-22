12fc7aaf1cd5c60692009c2e0ec7299ed90eff24
POLARBEARS-18 <r.magic.rx.8@gmail.com>
2022-09-22 19:02:24 +0900

    .

--- src/Apps/TrelloApp/TrelloHome.tsx
+++ src/Apps/TrelloApp/TrelloHome.tsx
@@ -3,18 +3,15 @@ import React from 'react';
 import { Header } from './components/Header';
 import { TaskCards } from './components/tasks/TaskCards';
 
-export const TrelloHome = () => {
-  const a = 'a';
-  return (
-    <div css={[SImg]}>
-      <Header />
-      <TaskCards />
-    </div>
-  );
-};
+export const TrelloHome = () => (
+  <div css={[SImg]}>
+    <Header />
+    <TaskCards />
+  </div>
+);
 
 const SImg = css`
-  background: url(../src/img/southern-country.jpeg);
+  background-image: url(../src/img/southern-country.jpeg);
   background-repeat: no-repeat;
   height: 100vh;
 `;
